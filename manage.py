3d45b2e63daa595e096376f4aae6ec7ac84a6543
leondante-ctrl <maxgamerdu@gmail.com>
2020-09-23 08:21:56 +0300

    set up a manage class to give user feedback on login

--- manage.py
+++ manage.py
@@ -1,28 +1,78 @@
-import manage as manage
-from app import db
-from app.models import User, Comment, Pitch
-from flask_migrate import Migrate, MigrateCommand
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
-app = creat_app('development')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-manager = Manager(app)
-manager.add_command('server', Server)
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_code = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comments = db.relationship('Comment', backref='user', lazy="dynamic")
 
-migrate = Migrate(app,db)
-manager.add_command('db', MigrateCommand)
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-@manager.MigrateCommand
-def test():
-    """
-    run the unittest
-    """
-    import unittest
-    test = unittest.TestLoader().discover('tests')
+    @password.setter
+    def password(self, password):
+        self.pass_code = generate_password_hash(password)
 
-@manager.shell
-def make_shell_context():
-    return dict(app = app, db = db, User = User)
+    def verify_password(self, password):
+        return check_password_hash(self.pass_code, password)
 
-# if __name__ == '__main__'
-manage.run()
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    pitch = db.Column(db.String)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitch_comments = db.relationship('Comment', backref='pitch', lazy="dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitch(cls, id):
+        pitch = Pitch.query.filter_by(id=id).first()
+
+        return pitch
+
+    def __repr__(self):
+        return f'User {self.title}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pitch_comment = db.Column(db.String)
+    pitch_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+        return comments
