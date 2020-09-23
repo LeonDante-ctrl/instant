62d50a475c0daad344f646b8bad16e05222caa51
leondante-ctrl <maxgamerdu@gmail.com>
2020-09-23 08:25:07 +0300

    debugged page

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from flask_login import User
 from . import login_manager
 from datetime import datetime
 
@@ -10,7 +10,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(UserMixin, db.Model):
+class User(User, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), index=True)
@@ -51,8 +51,8 @@ class Pitch(db.Model):
         db.session.commit()
 
     @classmethod
-    def get_pitch(cls, id):
-        pitch = Pitch.query.filter_by(id=id).first()
+    def get_pitch(cls, id1):
+        pitch = Pitch.query.filter_by(id=id1).first()
 
         return pitch
 
